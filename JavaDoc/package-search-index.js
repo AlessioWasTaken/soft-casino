320d10b561733d9e6662d8348bf7a49653f42f65
Alessio Sarica <sarica.alessio@outlook.it>
2023-05-11 18:12:06 +0200

    JavaDoc generato

---
+++ JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"App"},{"l":"auth"},{"l":"backend.auth"},{"l":"backend.blackjack"},{"l":"backend.corsaCavalli"},{"l":"backend.json"},{"l":"backend.slot"},{"l":"blackJack"},{"l":"corsaCavalli"},{"l":"Menu"},{"l":"shopPage"},{"l":"slotMachine"}];updateSearchResults();
