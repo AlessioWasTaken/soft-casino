d6d5408e0f06a26b0033f143f88384fe6bd8b674
Alessio <113789907+AlessioDevv@users.noreply.github.com>
2023-05-16 17:44:45 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('scroll', function(e) {
-  if(window.scrollY > 300){
-    document.body.classList.add('scroll-down');
-  } else {
-    document.body.classList.remove('scroll-down');
-  }
-});
